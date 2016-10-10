295de195e9cc3a693d44f943ea0c5853d894ca2e
haimao <haimao@xlingmao.com>
2016-10-10 09:45:00 +0800

    Initial Commit

---
+++ JCRouterDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JCRouterDemo
+//
+//  Created by seris-Jam on 16/10/10.
+//  Copyright © 2016年 seris-Jam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JCRouterDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JCRouterDemo
+//
+//  Created by seris-Jam on 16/10/10.
+//  Copyright © 2016年 seris-Jam. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
